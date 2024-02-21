1205465e1f1bbbe087a51fddef3dbdbb7fee80e7
FarhanMohammadDev <farhanmohammad0677@gmail.com>
2024-02-21 20:46:11 +0100

    add Lottie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+"use client"
+import Lottie from "lottie-react";
+import Entrance from '../lottie/LottieLego.json'
 export default function Home() {
   return (
     <>
+    <Lottie animationData={Entrance}/>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
